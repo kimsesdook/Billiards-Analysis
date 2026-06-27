08982ed9d0a13a35e3876b497d69a142a74585d9
HUNI6179\kimju <kimsesdook@gmail.com>
2026-06-27 17:55:57 +0900

    feat:백엔드 구조 추가

---
+++ Backend/src/main/java/com/health/GymQueue/GymQueueApplication.java
@@ -0,0 +1,13 @@
+package com.health.GymQueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymQueueApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/health/GymQueue/GymQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.GymQueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
