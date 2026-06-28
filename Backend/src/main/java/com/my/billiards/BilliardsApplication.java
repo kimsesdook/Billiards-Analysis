fc1a7127f88852965d6188475f478db7c7d2b7db
HUNI6179\kimju <kimsesdook@gmail.com>
2026-06-28 18:43:05 +0900

    feat : 백엔드 초기 구조 변경

---
+++ Backend/src/main/java/com/my/billiards/BilliardsApplication.java
@@ -0,0 +1,13 @@
+package com.my.billiards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilliardsApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/my/billiards/BilliardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.billiards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilliardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
